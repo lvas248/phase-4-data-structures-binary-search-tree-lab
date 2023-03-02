8348bedbd8c092ad6125835f9fb676b324f71444
Luis <lvas248@gmail.com>
2023-03-02 14:55:47 -0500

    commit tree 8c72b37a4f00c57f6ba7821f218669d9e6cff967

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class BinarySearchTree
   attr_accessor :root
@@ -8,11 +9,70 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+
+    node_to_visit = self.root
+    # test root node
+    # return node if value matches
+    # add node.left if given value is lass than node value
+    # add node.right if given value is greater than node value
+
+    while !node_to_visit.nil?
+
+      if node_to_visit.value == value
+        return node_to_visit
+      elsif value > node_to_visit.value
+        node_to_visit = node_to_visit.right
+      else
+        node_to_visit = node_to_visit.left
+      end
+      nil
+    end
+
   end
+
+
 
   def insert(value)
-    # your code here
-  end
+    new_node = Node.new(value)
+
+    # # create node and set equal to root node
+    # # test the values of new_node compared to node:
+    #   # if values are the same, return nil
+    #   # elsif new_node value is less than node.value
+    #     #if node.left exists, set node = node.left
+    #     #else node.left = new_node
+    #   # else new_node value is greater than node, set node equal to node.right
+    #     #if node.right exists, set node = node.right
+    #     #else node.right = new_node
+
+    if self.root.nil?
+      self.root = new_node
+    else
+        node = self.root
+        loop do
+          if node.value == new_node.value
+            return nil
+          elsif new_node.value < node.value
+            node.left ? ( node = node.left ) : ( 
+              node.left = new_node
+              return new_node)
+          else
+            node.right ? ( node = node.right ) : ( 
+              node.right = new_node
+              return new_node )
+          end
+        end
+      end
+    end
 
 end 
+
+# tree = BinarySearchTree.new
+
+# root = Node.new(1)
+
+# tree.root = root
+
+
+
+# binding.pry
